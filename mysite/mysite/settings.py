7f5a828a6a37794ee6ba22cc3dabe84881571fcc
sudo-ascend <grachevilia09@yandex.ru>
2025-07-25 17:27:11 +0300

    001

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myauth.apps.MyauthConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,16 +86,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.myauth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.myauth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.myauth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.myauth.password_validation.NumericPasswordValidator',
     },
 ]
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myauth.views import login
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
 ]
